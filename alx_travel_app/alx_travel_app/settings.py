e6d9b1b6cbecd8ee28c1664e53c0002f4383a265
Enobong Jackson <106785757+JasonSignatures@users.noreply.github.com>
2025-11-03 00:41:44 +0100

    Add environment variable configuration to settings
    Initialize environment variables for settings.

--- alx_travel_app/alx_travel_app/settings.py
+++ alx_travel_app/alx_travel_app/settings.py
@@ -1,1 +1,25 @@
+import environ
+import os
+
+# Initialize environment variables
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
+Update the settings:
+
+python
+Copy code
+DEBUG = env.bool("DEBUG", default=False)
+SECRET_KEY = env("SECRET_KEY")
+
+# Database
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': env('DB_HOST'),
+        'PORT': env('DB_PORT'),
+    }
+}
 
